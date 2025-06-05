26f83e2a1a908a3553849e7d424209504acba4a5
Black1151 <73132619+Black1151@users.noreply.github.com>
2025-06-05 12:23:23 +0100

    Add tab interface for Hospitality Hub admin

--- src/app/(site)/(apps-non-standard)/hospitality-hub/admin/components/HospitalityHubAdminClientInner.tsx
+++ src/app/(site)/(apps-non-standard)/hospitality-hub/admin/components/HospitalityHubAdminClientInner.tsx
@@ -1,5 +1,14 @@
-import { Text } from "@chakra-ui/react";
+"use client";
+
+import { PerygonTabs } from "../../big-up/tabs/PerygonTabs";
+
+const categories = ["Hotels", "Rewards", "Events", "Medical", "Legal"];
 
 export const HospitalityHubAdminClientInner = () => {
-  return <Text>Admin page here</Text>;
+  const tabsData = categories.map((category) => ({
+    header: category,
+    content: <></>,
+  }));
+
+  return <PerygonTabs tabs={tabsData} />;
 };
