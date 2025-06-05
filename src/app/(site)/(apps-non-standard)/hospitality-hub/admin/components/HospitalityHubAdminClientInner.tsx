a4db2e6d073963650eb647dca1795c26676b7c74
Black1151 <danblack9988@gmail.com>
2025-06-05 12:25:22 +0100

    fix import

--- src/app/(site)/(apps-non-standard)/hospitality-hub/admin/components/HospitalityHubAdminClientInner.tsx
+++ src/app/(site)/(apps-non-standard)/hospitality-hub/admin/components/HospitalityHubAdminClientInner.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { PerygonTabs } from "../../big-up/tabs/PerygonTabs";
+import { PerygonTabs } from "../../../big-up/tabs/PerygonTabs";
 
 const categories = ["Hotels", "Rewards", "Events", "Medical", "Legal"];
 
