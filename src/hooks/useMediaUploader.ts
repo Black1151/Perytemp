034fcfc9a628b345cda3959041aaa14eafb82294
Black1151 <73132619+Black1151@users.noreply.github.com>
2025-06-10 12:24:41 +0100

    feat(hospitality-hub): add image upload

--- src/hooks/useMediaUploader.ts
+++ src/hooks/useMediaUploader.ts
@@ -73,7 +73,7 @@ export const useMediaUploader = (
       const resp = await fetch(endpoint, { method: "POST", body: formData });
       if (!resp.ok) throw new Error("Failed to upload file");
 
-      await resp.json();
+      const data = await resp.json();
       toast({
         title: "Upload successful",
         description: "Your file has been uploaded.",
@@ -83,6 +83,7 @@ export const useMediaUploader = (
         position: "bottom-right",
       });
       onSuccess();
+      return data;
     } catch (err) {
       console.error("File upload error:", err);
       toast({
